5c0603cbe978a526829908f62abe7d0e7055f43d
AditiCode244 <saxenaaditii123@gmail.com>
2026-01-24 16:04:57 +0530

    Create retail_sales_analysis.sql

---
+++ sql/retail_sales_analysis.sql
@@ -0,0 +1,10 @@
+ Retail Sales Analysis using SQL
+-- Status: Work in Progress
+
+-- Dataset contains:
+-- customers
+-- transactions
+-- products
+-- stores
+
+-- Queries will be added step by step
